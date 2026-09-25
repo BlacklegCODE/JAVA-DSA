35de17cb37ef9177fc1ad92688a09368c0759202
Harsh Raundal <dkingston0000@gmail.com>
2026-09-25 17:04:04 +0530

    Add BubbleSort implementation in BubbleSort.java
    Implement Bubble Sort algorithm to sort an array of integers.

---
+++ BubbleSort.java
@@ -0,0 +1,36 @@
+//Bubble sort
+import java.util.*;
+class first
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size :");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter " + n+" Elements :");
+        for(int i = 0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = 0;j<n-1-i;j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        for(int i = 0;i<n;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
