d2a05edf20937d653d08f44c37de12eb3917c526
Harsh Raundal <dkingston0000@gmail.com>
2026-09-26 23:22:59 +0530

    Create RecursionBasics.java

---
+++ RecursionBasics.java
@@ -0,0 +1,17 @@
+//Recursion practice
+class first
+{
+    public static void printN(int n)
+    {
+        if(n == 0){
+         return ;
+        }
+        System.out.print(n + " ");
+        printN(n-1);
+    }
+    public static void main(String[] args)
+    {
+        int n = 5;
+        printN(n);
+    }
+}
